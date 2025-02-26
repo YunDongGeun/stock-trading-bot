339eb181665b4e194b3b866b7c7e5997c0ae4cc0
YunDongGeun <dong@geun.kr>
2025-02-27 03:04:32 +0900

    first commit

--- pandas_ex/data_handling_1.py
+++ pandas_ex/data_handling_1.py
@@ -1,8 +1,19 @@
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.DataFrame()
-df["Name"] = ["ydg", "lms", "cde"]
-df["Age"] = [21, 32, 24]
-df["Driver"] = [True, False, True]
+# 2x2 서브플롯 생성
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 3))
 
-df
+# 첫 번째 서브플롯 설정
+ax1.plot(np.random.rand(50), color='red')
+ax1.set_title('Plot 1')
+
+# 두 번째 서브플롯 설정
+ax2.bar(np.arange(10), np.random.rand(10))
+ax2.set_title('Plot 2')
+
+# 레이아웃 조정
+plt.tight_layout()
+
+# 피겨 표시
+plt.show()
